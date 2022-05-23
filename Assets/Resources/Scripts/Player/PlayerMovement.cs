0f788b8bf659220ca41c4c8bea94a1fdb0b02689
Ryan <bolsonryan@gmail.com>
2022-05-23 19:56:34 +0100

    Added two colliders to player.
    - Added "Ground" LayerMask.
    - Implemented a ground check in PlayerMovement.cs.
    - Added "Ground" GameObject to the scene.

--- Assets/Resources/Scripts/Player/PlayerMovement.cs
+++ Assets/Resources/Scripts/Player/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -8,12 +7,30 @@ namespace Resources.Scripts.Player
 {
     public class PlayerMovement : MonoBehaviour{
         private Rigidbody2D _rigidbody2D;
+        [SerializeField] private Transform _groundCheck;
+        [SerializeField] private LayerMask _groundLayerMask;
 
-        public UnityEvent OnLandEvent; 
+        private const float _groundedRadius = 0.2f;
+        private bool _isGrounded;
+        public UnityEvent OnLandEvent;
 
         private void Awake(){
             if (OnLandEvent == null)
                 OnLandEvent = new UnityEvent();
+        }
+
+        private void FixedUpdate(){
+
+            // Store all colliders within ground-check's radius, on the ground layer:
+            Collider2D[] collider2Ds = Physics2D.OverlapCircleAll(
+                _groundCheck.position,
+                _groundedRadius,
+                _groundLayerMask
+            );
+            foreach (var colliderArg in collider2Ds){
+                if (colliderArg.gameObject != gameObject)
+                    _isGrounded = true;
+            }
         }
     }
 }
