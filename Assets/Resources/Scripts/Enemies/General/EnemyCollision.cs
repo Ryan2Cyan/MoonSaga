abdb7e1a7792c7598b2d52298ce6f0db9add9c31
Ryan <bolsonryan@gmail.com>
2022-06-03 20:44:14 +0100

    Implemented simpler enemy edge checker in "EnemyCollision.cs".
    - Added "Edge-0" and "Edge-1" GameObjects.

--- Assets/Resources/Scripts/Enemies/General/EnemyCollision.cs
+++ Assets/Resources/Scripts/Enemies/General/EnemyCollision.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 namespace Resources.Scripts.Enemies.General{
     public class EnemyCollision : MonoBehaviour{
 
+        // Scripts:
+        [SerializeField] private EnemyMovement _enemyMovementScript;
         // Trigger collider:
         [SerializeField] internal Collider2D _boxCollider;
 
@@ -14,6 +16,13 @@ namespace Resources.Scripts.Enemies.General{
             
             // Ignore collisions with player:
             Physics2D.IgnoreCollision(player.GetComponent<CircleCollider2D>(), GetComponent<Collider2D>());
+        }
+
+        private void OnCollisionEnter2D(Collision2D other){
+            if (other.gameObject.CompareTag("PlatformEdge")){
+                _enemyMovementScript._isFacingRight = !_enemyMovementScript._isFacingRight;
+                Debug.Log("Hit Edge");
+            }
         }
     }
 }
